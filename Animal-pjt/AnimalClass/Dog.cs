7124ef10021b1c3030ba1ac888ca8ceb1a06ecca
MuRashad <mustafarashad078@gmail.com>
2024-07-20 02:21:51 +0300

    i have added project to practise inheritance the name of the class Animal  and has console to write and read.

---
+++ Animal-pjt/AnimalClass/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_pjt.AnimalClass
+{
+    //i maake inheritance 
+    public class Dog :Animal 
+    {
+        private string _type;
+        public string _Type
+
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        //i can make ctor in the children if the parent have parmatrless costructor 
+        //if doesnot have parameterless constructor i have to pass to base class constructor parameters 
+        //here i make initialization to class from here parent to DOfg but not setting values or getting it 
+        public Dog(string type):base("Black German","Germany","blac and yellow" ,"has tail" ,4)
+        {
+            _type = type;
+            Console.WriteLine($"this is parametrize consrtructor for childeren {this._Name},...!");
+        }
+        public new string ToString()
+        {
+            return $"the type of Dog is {_type}";
+        }
+    } 
+}
